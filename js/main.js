31dfa53a435ffac34d237963831cdcfd2795e53e
bcd82 <barak.sidi@gmail.com>
2021-08-13 15:46:28 +0300

    revert

--- js/main.js
+++ js/main.js
@@ -196,6 +196,9 @@ const upload = async (response, page_token) => {
 };
 
 async function clickShare()  {
+    gIsDownloading = true;
+    renderCanvas()
+    gIsDownloading = false;
     const dataUrl = gElCanvas.toDataURL();
     const blob = await (await fetch(dataUrl)).blob();
     const filesArray = [
